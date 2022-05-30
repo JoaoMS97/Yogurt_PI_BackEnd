045ed07bce75aab27f2f96f7bf6bd4379a835625
JoaoMS97 <joaobusht@gmail.com>
2022-05-30 20:04:25 -0300

    Refaturações

--- Yogurt.Application/Utils/Utilitarios.cs
+++ Yogurt.Application/Utils/Utilitarios.cs
@@ -1,5 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
 using System.Security.Cryptography;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Yogurt.Application.Utils
 {
@@ -19,12 +23,31 @@ namespace Yogurt.Application.Utils
 
         public static bool VerificarEmail(string? email)
         {
-            if(new EmailAddressAttribute().IsValid(email))
+            if (new EmailAddressAttribute().IsValid(email))
             {
                 return true;
             }
 
             return false;
+        }
+
+       public static string Testar()
+        {
+            MySqlConnection conexao = new MySqlConnection("server=ns480.hostgator.com.br;User Id=clowto23_User ;database=clowto23_Jovem_Programador; password=DiamantedoRegis.");
+
+            MySqlCommand comando = new MySqlCommand("insert into clowto23_Jovem_Programador.Usuario () column Idade", conexao);
+            DataTable tabela = new DataTable();
+            try
+            {
+                conexao.Open();
+                comando.ExecuteReader();
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+            return "";
         }
     }
 }
